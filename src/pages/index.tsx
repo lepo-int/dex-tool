33eec2fa938271e6dcf66bac2e55784e84834794
DESKTOP-MGG0G72\Administrator <seniordev416@gmail.com>
2025-03-16 09:18:53 -0400

    add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,11 @@ export default function Home() {
     <Layout>
       <div className="flex mx-90 my-20">
         <div className="text-white mt-10">
-          <h1 className="text-8xl">Bounty Swap</h1>
+          <h1 className="text-8xl"><strong>Bounty</strong> Swap</h1>
           <p className="text-2xl mt-5"><strong>Trade crypto</strong> seamlessly with our DEX App.</p>
           <p className="text-2xl mt-2"><strong>Fast, Secure, and Fully Decentralized</strong>.</p>
           <p className="text-2xl mt-2">Swap assets anytime with <strong>low fees and no middlemen! </strong></p>
-          <button className="font-bold cursor-pointer border-2 bg-gradient-to-r from-blue-400 to-blue-600 my-5 px-8 py-2 rounded-full text-white hover:bg-blue-500">Start Trading</button>
+          <button className="font-bold cursor-pointer border-2 bg-gradient-to-r from-blue-400 to-blue-600 my-5 px-8 py-2 rounded-full text-white text-lg hover:bg-blue-500">Start Trading</button>
           <div className="mt-15">
             <div className="flex space-x-10">
               <PriceSeg network={"ethereum"} />
